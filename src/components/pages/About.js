a053f313747a939268cd8d88179d3d0da0b71c7a
Conner Iraola <conner_iraola@yahoo.com>
2022-07-23 15:11:59 -0700

    updates to src

---
+++ src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <div class="about-me">
+            <img src="./assets/images/LuffySmile.jpg" alt="Luffy smiling" />
+            <h1>About Me</h1>
+            <p>
+                Hi! My name is Luffy! Errrr, I mean Chris and I am a ScrumMaster who wishes to pick up full stack web development skills for work and personal. I have an awesome tutor as well.
+            </p>
+            <p>
+                I love learning new things, so that may be what pulled me into signing up for this coding boot camp. I am very interested in building a desktop application with Electron.js!
+            </p>
+            <p>One day I will become find the treasure One Piece and become the Pirate King!</p>
+
+        </div>
+    );
+}
---
+++ src/components/pages/Projects.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Projects() {
+  return (
+    <div>
+      <h1>Projects Page</h1>
+      <p>
+        <ul>
+            <a href = "https://github.com/MrTofuuu/CAPES"><li>Civillian and Planetary Emergency Services</li></a>
+        </ul>
+        <ul>
+            <a href = "https://github.com/MrTofuuu/bounty-board"><li>Bounty Board</li></a>
+        </ul>
+        <ul>
+            <a href = "https://github.com/MrTofuuu/nakama-tracker"><li>Nakama Tracker</li></a>
+        </ul>
+        <ul>
+            <a href = "https://github.com/MrTofuuu/nakama-info-generator"><li>Nakama Information Generator</li></a>
+        </ul>
+      </p>
+    </div>
+  );
+}
